6496197146fc04a453aabff199ed9f5a442614dc
Doudou <adeldoudou1996@gmail.com>
2020-04-09 12:20:45 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,15 +4,15 @@
 // Connexion à la base de données
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
-$req=$bdd->prepare('INSERT INTO user(nom)VALUES(?)');
-$req->execute( array($_POST['user']) );
+$req=$bdd->prepare('INSERT INTO user(name) VALUES(?)');
+$req->execute(array($_POST['user']));
 
 ?>
  
