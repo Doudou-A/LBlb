3568d48034f14035d73cf717eda12e2f7bfbf0c8
Doudou <adeldoudou1996@gmail.com>
2020-04-09 15:27:41 +0200

    pp

--- view/indexView.php
+++ view/indexView.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 
-<a class="btn btn-outline-success my-2 my-sm-0 text-white" href="index.php?action=test">Changement de vue</a>
+<a class="btn btn-outline-success my-2 my-sm-0 bg-primary" href="index.php?action=test">Changement de vue</a>
 
 <?php $content = ob_get_clean(); ?>
 
